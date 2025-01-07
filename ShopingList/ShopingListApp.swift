d827620e7d5e704817052c608da3f49ebf2b5097
Serhii Tkachenko <doker.tk@gmail.com>
2025-01-07 23:06:37 +0100

    Initial Commit

---
+++ ShopingList/ShopingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopingListApp.swift
+//  ShopingList
+//
+//  Created by Serhii on 07.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShopingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
