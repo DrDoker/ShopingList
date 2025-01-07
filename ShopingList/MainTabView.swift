fa858cb1a493946ffc3308e1bd3f9dda706c9326
Serhii Tkachenko <doker.tk@gmail.com>
2025-01-07 23:42:21 +0100

    Start

---
+++ ShopingList/MainTabView.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabView.swift
+//  ShopingList
+//
+//  Created by Serhii on 07.01.2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            NavigationStack {
+                ShopingListCatalogView()
+            }
+            .tabItem {
+                Label("Shoping List", systemImage: "checklist")
+            }
+        }
+        .accentColor(.yellow)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
--- ShopingList/ShopingListApp.swift
+++ ShopingList/ShopingListApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct ShopingListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
+                .preferredColorScheme(.dark)
         }
     }
 }
